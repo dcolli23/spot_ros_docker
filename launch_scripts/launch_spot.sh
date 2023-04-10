635d775ebc17a9464783ca996b6663404b94c587
Dylan Colli <dylanfrankcolli@gmail.com>
2023-04-10 16:12:12 -0400

    Update main launch script to include help message
    - Also implements basic logic for checking that the correct parameter
    was passed (the Spot USER password).

--- launch_scripts/launch_spot.sh
+++ launch_scripts/launch_spot.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-# Launches all necessary components to enable ROS-based control of Spot. This is accomplished by
-# launching 3 persistent terminals (meaning they stick around after this script is done executing)
-# that do the following:
-# 1. Starts OpenVPN on Spot CORE
-# 2. Starts the ROS driver on Spot via launching docker container
-# 3. Starts OpenVPN on operator PC
-#
-# NOTE: This becomes vastly more convenient if you have SSH keys setup between the operator PC and
-# the Spot CORE.
+HELP_MSG=$(cat <<-END
+Launches all necessary components to enable ROS-based control of Spot. This is accomplished by
+launching 3 persistent terminals (meaning they stick around after this script is done executing)
+that do the following:
+1. Starts OpenVPN on Spot CORE
+2. Starts the ROS driver on Spot via launching docker container
+3. Starts OpenVPN on operator PC
+
+NOTE: This becomes vastly more convenient if you have SSH keys setup between the operator PC and
+the Spot CORE.
+
+Positional arguments:
+    spot_user_password
+        The USER password for Spot. This is found in the battery compartment of your Spot.
+Named arguments:
+    -h, --help
+        Displays this message.
+END
+)
 
 # Set up constants to make the script a bit more readable.
 SPOT_CORE_PORT='20022'
@@ -16,6 +26,17 @@ SPOT_CORE_PORT='20022'
 # Setting up variables so that the option of passing in non-default arguments is easier down the
 # road.
 ROBOT_IP='192.168.80.3'
+
+# Do some basic checking of command line arguments.
+if [ $# -eq 0 ]; then
+    echo "$HELP_MSG"
+    echo
+    echo "ERROR! spot_user_password is a required argument!"
+    exit 1
+elif [ $1 = "-h" ] || [ $1 = "--help" ]; then
+    echo "$HELP_MSG"
+    exit 0
+fi
 
 # Read the password from the command line arguments passed into this script.
 SPOT_USER_PASSWORD=$1
